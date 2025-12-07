844ef3a9e2ce8ca2288077719c73d86824641323
raspy <lyukey0902@gmail.com>
2025-12-07 18:13:33 +0900

    practice: C++、基本構文、ラムダ式

--- tutorial/main.cpp
+++ tutorial/main.cpp
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#include <iostream>
+#include <functional>
 
-void func(char* s) {
-    printf("引数で渡された文字列: %s\n", s);
+int Add(int x, int y) {
+    return x + y;
 }
 
-int main(void) {
-    char str[] = "dog";
-    func(str);
+int main() {
+    std::function<int(int, int)> f = Add;
+    int result = f(3, 5);
+    std::cout << result << std::endl;
+
+    f = [](int x, int y) { return x * y; };  // ラムダ式を代入
+    std::cout << f(4, 7) << std::endl;
+
+    return 0;
 }
