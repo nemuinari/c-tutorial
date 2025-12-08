c80f2b0c4655be486c931bb684ea9bb6b721dfb3
raspy <lyukey0902@gmail.com>
2025-12-08 10:13:34 +0900

    practice: コンテナ

--- tutorial/main.cpp
+++ tutorial/main.cpp
@@ -3,10 +3,21 @@
 #include <set>
 
 int main() {
-    std::set<int> x = {0, 1, 2, 3, 4};
+    std::vector<int> x = {0, 1, 2, 3, 4};
 
+    /* for文
     for (auto it = x.begin(); it != x.end(); ++it) {
         std::cout << *it << std::endl;
+    } */
+
+    /* 範囲for文
+    for (auto&& e : x) {
+        std::cout << e << std::endl;
+    } */
+
+    // C++23 範囲for文: シンプルに書ける
+    for (auto e : x) {
+        std::cout << e << std::endl;
     }
 
     return 0;
