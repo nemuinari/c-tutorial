94395b31eee762d480ab1d01cad78bbe3cb5cae0
raspy <lyukey0902@gmail.com>
2025-12-22 09:33:51 +0900

    practice: atcoder

--- atcoder/main.cpp
+++ atcoder/main.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 int main() {
-    std::string S;
-    std::cin >> S;
+    int H, W;
 
-    std::cout << S + "pp" << std::endl;
+    std::cin >> H >> W;
+    std::vector<std::vector<char>> a(H + 2, std::vector<char>(W + 2, '#'));
 
+    for(int i = 1; i <= H; i++) {
+        for(int j = 1; j <= W; j++) {
+            std::cin >> a.at(i).at(j);
+        }
+    }
+    for(int i = 0; i < H + 2; i++) {
+        for(int j = 0; j < W + 2; j++) {
+            std::cout << a.at(i).at(j);
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
-// M - A - ハンドルネーム 
+// N B - PictureFrame
