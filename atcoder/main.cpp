d04e7bf76e5fd000d8b7e484b4618c9bff88ee4d
raspy <lyukey0902@gmail.com>
2026-01-04 17:30:26 +0900

    practice: atcoder

--- atcoder/main.cpp
+++ atcoder/main.cpp
@@ -1,19 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> data(5);
-    for (int i = 0; i < 5; ++i) {
-        cin >> data.at(i);
+int main()
+{
+    int N, S;
+    cin >> N >> S;
+
+    vector<int> A(N), P(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A.at(i);
+    }
+    for (int i = 0; i < N; i++)
+    {
+        cin >> P.at(i);
     }
 
-    for (size_t i = 1; i < data.size(); i++) {
-        if (data.at(i - 1) == data.at(i)) {
-            cout << "YES" << endl;
-            return 0;
+    // リンゴ・パイナップルをそれぞれ1つずつ購入するとき合計S円になるような買い方が何通りあるか
+    // ここにプログラムを追記
+    int count = 0;
+
+    for (int x : A)
+    {
+        for (int y : P)
+        {
+            if (x + y == S)
+            {
+                count++;
+            }
         }
     }
-    cout << "NO" << endl;
+    cout << count << endl;
+
     return 0;
 }
-// Q - EX16
+// S - EX17
