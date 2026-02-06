b5b06ca82c78a3c3348de79d81e0e86b631031b1
nemuirari <lyukey0902@gmail.com>
2026-02-06 10:32:08 +0900

    practice: c-language

---
+++ _archive/ch02-05.c
@@ -0,0 +1,17 @@
+// chapter 2-1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        puts("Hello World");
+    } else {
+        int sum = 0;
+        for (int i = 1; i < argc; i++) {
+            sum += atoi(argv[i]);
+        }
+        printf("sum: %d\n", sum);
+    }
+    return 0;
+}
---
+++ ch06-02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int n = 0;
+    do {
+        n += 1;
+    } while (++n <= 0);
+    printf("%i\n", n);  // Output: 2
+
+    return 0;
+}
